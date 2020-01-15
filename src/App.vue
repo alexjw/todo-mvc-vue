dc9996ac5d3ba3a5951b3ae72d66b8fca5e0d5b0
Alex J <abel.oalex@gmail.com>
2020-01-15 16:28:46 -0300

    added functionality

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,39 @@
         <h1>
             Hey!!asdddddddddxxxx
         </h1>
-        <ul v-for="todo in $data.todos">
-            <li>
+        <ul v-for="todo in $data.todos.filter(shouldShowToDo)">
+            <li v-bind:class="{complete: todo.done}">
                 {{todo.text}}
+                <input type="checkbox" v-model="todo.done">
             </li>
         </ul>
+        <form v-on:submit="handleAddTodo($event)">
+            <input type="text" v-model="input">
+        </form>
+        <label class="show-completed">
+            Show Completed Items?
+            <input type="checkbox" v-model="showDone">
+        </label>
     </div>
 </template>
 
 
 <script>
     export default {
+        methods: {
+            handleAddTodo(e) {
+                e.preventDefault();
+                this.$data.todos.push({text:this.input, done:false});
+                this.input = "";
+            },
+            shouldShowToDo(todo) {
+                if(this.$data.showDone) {
+                    return true;
+                } else {
+                    return !todo.done;
+                }
+            }
+        },
         data() {
             return this.$props.state;
         },
@@ -23,5 +45,9 @@
 
 
 <style>
-
+    .complete {
+        color: gainsboro;
+        text-decoration: line-through;
+        font-style: italic;
+    }
 </style>
