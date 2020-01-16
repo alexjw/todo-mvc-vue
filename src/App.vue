743c5eedace9c0c2feb42aca3fa61e69712fb1bf
Alex J <abel.oalex@gmail.com>
2020-01-16 11:17:08 -0300

    added css style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>
-            Hey!!asdddddddddxxxx
+            Vue to-do App
         </h1>
         <ul v-for="todo in $data.todos.filter(shouldShowToDo)">
             <li v-bind:class="{complete: todo.done}">
@@ -45,9 +45,50 @@
 
 
 <style>
+    body {
+        font-family: Helvetica, sans-serif;
+        color: darkslategray;
+        font-size: 1.4em;
+        margin: 1.4em;
+        width: 22em;
+        position: relative;
+    }
+    input[type=text] {
+        margin-top: 1em;
+        padding: 0.8em;
+        width: 100%;
+        font-size: 0.7em;
+    }
+    input[type=checkbox] {
+        position: absolute;
+        right: 0;
+        padding: 1em;
+    }
+    li {
+        list-style-type: none;
+        padding: 1em 0 1em 0;
+        border-bottom: 1px solid lightblue;
+        position: relative;
+    }
+    li input {
+        transform: scale(1.8);
+    }
+    ul {
+        padding: 0;
+    }
+    label {
+        width: 80%;
+    }
     .complete {
         color: gainsboro;
         text-decoration: line-through;
         font-style: italic;
+    }
+    .show-completed {
+        font-size: 1rem;
+        color: slategray;
+    }
+    .show-completed input {
+        transform: scale(1.4);
     }
 </style>
